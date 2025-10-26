e27a8450cfc2030470589792ba0e7c6bbf21f405
JulianAndresNaranjo <142056387+JulianAndresNaranjo@users.noreply.github.com>
2025-10-26 17:46:12 -0500

    Update db.js

--- db.js
+++ db.js
@@ -4,12 +4,13 @@ import dotenv from 'dotenv'
 dotenv.config()
 const { Pool } = pkg
 
+console.log('🔎 DATABASE_URL =>', process.env.DATABASE_URL || '(no definida)')
+
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 })
 
-// Cambiamos el esquema por defecto a "vitalapp"
 pool.on('connect', async client => {
   await client.query('SET search_path TO vitalapp;')
 })
